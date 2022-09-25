f4215b012080a4f14e3118abd41dd51a0f5963a2
Bernard Ofori <ofori.nakawah@gmail.com>
2022-09-25 21:24:35 +0000

    finalizing setup

---
+++ app/Http/Controllers/OnboardingController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OnboardingController extends Controller
+{
+    public function register(Request $request)
+    {
+
+    }
+
+    public function validateEmail(Request $request)
+    {
+
+    }
+
+    public function validatePhoneNumber(Request $request)
+    {
+
+    }
+}
