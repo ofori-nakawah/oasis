edb8276248c98728e2867992cca194d1fc59a1f0
Bernard Ofori <ofori.nakawah@gmail.com>
2023-05-12 17:32:19 +0000

    integrate hubtel sms

--- app/Services/SMS.php
+++ app/Services/SMS.php
@@ -8,27 +8,53 @@ use Illuminate\Support\Facades\Log;
 
 class SMS
 {
+    /**
+     * set service provider in the code using PRs
+     * value should be either HUBTEL | SMS_BOLT
+     */
+    const SMS_PROVIDER = "HUBTEL";
+
     public static function notify($phone,$message){
-        $message = urlencode($message);
         $phone = self::format_msisdn($phone);
 
         Log::debug("SENDING SMS TO >>>>>>>> $phone");
 
         $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "http://smsbolt.com/sms/api?action=send-sms&api_key=dm9yay10ZWNoOkBDMGQzTjFuamE=&to=$phone&from=VORK&sms=$message",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-                "postman-token: 4134a3ec-0a36-18f3-f24a-7c91f27f78e9"
-            ),
-        ));
+        switch (self::SMS_PROVIDER) {
+            case "HUBTEL":
+                $query = array(
+                    "clientid" => env("HUBTEL_CLIENT_ID"),
+                    "clientsecret" => env("HUBTEL_CLIENT_SECRET"),
+                    "from" => "VORK",
+                    "to" => $phone,
+                    "content" => $message
+                );
+
+                curl_setopt_array($curl, [
+                    CURLOPT_URL => "https://devp-sms03726-api.hubtel.com/v1/messages/send?" . http_build_query($query),
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                ]);
+                break;
+            case "SMS_BOLT":
+                $message = urlencode($message);
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "http://smsbolt.com/sms/api?action=send-sms&api_key=dm9yay10ZWNoOkBDMGQzTjFuamE=&to=$phone&from=VORK&sms=$message",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_HTTPHEADER => array(
+                        "cache-control: no-cache",
+                        "postman-token: 4134a3ec-0a36-18f3-f24a-7c91f27f78e9"
+                    ),
+                ));
+                break;
+        }
 
         $response = curl_exec($curl);
         $error = curl_error($curl);
