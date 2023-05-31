646d7f1c7adb9c54aad333088d6b88effccaf31b
Bernard Ofori <ofori.nakawah@gmail.com>
2023-05-31 19:21:22 +0000

    replace analytics

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -29,7 +29,9 @@
                     class="@if($user->status == 1) text-success @elseif($user->status == 2) text-warning @else text-secondary @endif"><em
                         class="icon ni ni-circle-fill @if($user->status == 1) text-success @elseif($user->status == 2) text-warning @else text-secondary @endif"></em> {{($user->status == 1) ? 'active' : 'inactive'}}</span>
                 <span class="text-primary"><em class="icon ni ni-live text-primary"></em> Member since {{$user->created_at}}</span>
-                <span><em class="icon ni ni-eye"></em> @if($user->is_online) <span class="text-success">online</span> @else Last seen was {{($user->last_seen) ? $user->last_seen->diffForHumans() : ' a while back'}} @endif</span>
+                <span><em class="icon ni ni-eye"></em> @if($user->is_online) <span
+                        class="text-success">online</span> @else Last seen
+                    was {{($user->last_seen) ? $user->last_seen->diffForHumans() : ' a while back'}} @endif</span>
             </div>
         </div>
     </div>
@@ -40,57 +42,70 @@
                 <div class="card-body" style="padding: 0px;height:  270px;">
                     @if($user->profile_picture)
                         <div class="text-center">
-                            <img src="{{$user->profile_picture}}" alt="" style="height: 220px;width: 220px;border: 1px solid #ccc;border-radius: 50%;margin-top: 25px;">
-                            <div>@if($user->id == auth()->user()->id) <a href="#"  data-toggle="modal" data-target="#editProfilePicModal"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</div>
+                            <img src="{{$user->profile_picture}}" alt=""
+                                 style="height: 220px;width: 220px;border: 1px solid #ccc;border-radius: 50%;margin-top: 25px;">
+                            <div>@if($user->id == auth()->user()->id) <a href="#" data-toggle="modal"
+                                                                         data-target="#editProfilePicModal"><em
+                                        class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</div>
 
                         </div>
                     @else
                         <div style="margin-top: 80px;">
                             <em class="icon ni ni-user" style="font-size: 105px"></em>
-                            <div>@if($user->id == auth()->user()->id) <a href="#"  data-toggle="modal" data-target="#editProfilePicModal"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</div>
+                            <div>@if($user->id == auth()->user()->id) <a href="#" data-toggle="modal"
+                                                                         data-target="#editProfilePicModal"><em
+                                        class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</div>
                         </div>
                     @endif
                 </div>
                 <div class="card-footer bg-white border-top" style="padding-left: 0px;padding-right: 0px;;">
-                    <div><b>{{$user->name}} @if($user->id == auth()->user()->id) <a href="#"  data-toggle="modal" data-target="#editNameModal"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</b></div>
+                    <div><b>{{$user->name}} @if($user->id == auth()->user()->id) <a href="#" data-toggle="modal"
+                                                                                    data-target="#editNameModal"><em
+                                    class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</b></div>
                     <hr>
-                    <div><em class="icon ni ni-map-pin"></em>{{$user->location_name}} @if($user->id == auth()->user()->id) <a href="{{route("user.profile.updateLocation")}}"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</div>
+                    <div><em
+                            class="icon ni ni-map-pin"></em>{{$user->location_name}} @if($user->id == auth()->user()->id)
+                            <a href="{{route("user.profile.updateLocation")}}"><em class="icon ni ni-pen"></em>
+                                <b>Edit</b></a> @endif</div>
                 </div>
             </div>
 
             <br>
 
-            <form action="{{route("user.updateProfileInformation", ["module" => "display-name"])}}" method="POST" enctype="multipart/form-data">
+            <form action="{{route("user.updateProfileInformation", ["module" => "display-name"])}}" method="POST"
+                  enctype="multipart/form-data">
                 {{csrf_field()}}
-            <div class="modal modal-lg fade" tabindex="-1" id="editNameModal">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <a href="#" class="close" data-dismiss="modal" aria-label="Close">
-                            <em class="icon ni ni-cross"></em>
-                        </a>
-                        <div class="modal-header">
-                            <h5 class="modal-title">Update your name</h5>
-                        </div>
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-control-wrajhgp">
-                                        <input type="text" class="form-control form-control-lg"
-                                               id="name" name="name"
-                                               placeholder="Enter your new name">
+                <div class="modal modal-lg fade" tabindex="-1" id="editNameModal">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <a href="#" class="close" data-dismiss="modal" aria-label="Close">
+                                <em class="icon ni ni-cross"></em>
+                            </a>
+                            <div class="modal-header">
+                                <h5 class="modal-title">Update your name</h5>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-control-wrajhgp">
+                                            <input type="text" class="form-control form-control-lg"
+                                                   id="name" name="name"
+                                                   placeholder="Enter your new name">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="modal-footer bg-white text-right" style="float: right !important;">
-                            <button style="float: right;" class="btn btn-outline-primary"><b>Save Changes</b></button>
+                            <div class="modal-footer bg-white text-right" style="float: right !important;">
+                                <button style="float: right;" class="btn btn-outline-primary"><b>Save Changes</b>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
 
-            <form action="{{route("user.updateProfileInformation", ["module" => "profile-picture"])}}" method="POST" enctype="multipart/form-data">
+            <form action="{{route("user.updateProfileInformation", ["module" => "profile-picture"])}}" method="POST"
+                  enctype="multipart/form-data">
                 {{csrf_field()}}
                 <div class="modal modal-lg fade" tabindex="-1" id="editProfilePicModal">
                     <div class="modal-dialog" role="document">
@@ -102,7 +117,8 @@
                                 <h5 class="modal-title">Update your profile picture</h5>
                             </div>
                             <div class="modal-body">
-                                <p style="text-align: left;">Choose a picture you want to use as your profile picture</p>
+                                <p style="text-align: left;">Choose a picture you want to use as your profile
+                                    picture</p>
                                 <div class="row">
                                     <div class="col-md-12">
                                         @if($user->profile_picture)
@@ -117,14 +133,16 @@
 
                                         <div class="form-control-wrajhgp">
                                             <input type="file" class="form-control form-control-lg"
-                                                   id="profile_picture" name="profile_picture" accept="image/png, image/gif, image/jpeg, image/jpg"
+                                                   id="profile_picture" name="profile_picture"
+                                                   accept="image/png, image/gif, image/jpeg, image/jpg"
                                                    placeholder="Choose a picture">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="modal-footer bg-white text-right" style="text-align: right !important;">
-                                <button style="float: right;" class="btn btn-outline-primary"><b>Save Changes</b></button>
+                                <button style="float: right;" class="btn btn-outline-primary"><b>Save Changes</b>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -137,24 +155,30 @@
                     <div id="languagesInterestsBox">
                         <div class="card card-bordered bg-white">
                             <div class="card-header bg-white border-bottom">
-                                <b>Skills & Interests @if($user->id == auth()->user()->id) <a href="{{route("user.profile.skills_and_interest")}}"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</b>
+                                <b>Skills & Interests @if($user->id == auth()->user()->id) <a
+                                        href="{{route("user.profile.skills_and_interest")}}"><em
+                                            class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</b>
                             </div>
                             <div class="card-body" style="height: 125px;padding-top: 10px;padding-bottom: 10px;">
                                 @foreach($skills as $skill)
                                     <span
-                                        class="badge badge-md badge-dim badge-pill badge-outline-dark" style="margin-bottom: 5px;">{{$skill->skill->name}}</span>
+                                        class="badge badge-md badge-dim badge-pill badge-outline-dark"
+                                        style="margin-bottom: 5px;">{{$skill->skill->name}}</span>
                                 @endforeach
                             </div>
                         </div>
                         <br>
                         <div class="card card-bordered bg-white" style="margin-top: 5px;">
                             <div class="card-header bg-white border-bottom">
-                                <b>Languages  @if($user->id == auth()->user()->id) <a href="{{route("user.profile.languages")}}"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif</b>
+                                <b>Languages @if($user->id == auth()->user()->id) <a
+                                        href="{{route("user.profile.languages")}}"><em class="icon ni ni-pen"></em> <b>Edit</b></a> @endif
+                                </b>
                             </div>
                             <div class="card-body" style="height: 125px;padding-top: 10px;padding-bottom: 10px;">
                                 @foreach($languages as $language)
                                     <span
-                                        class="badge badge-md badge-dim badge-pill badge-outline-dark" style="margin-bottom: 5px;">{{$language->language->name}}</span>
+                                        class="badge badge-md badge-dim badge-pill badge-outline-dark"
+                                        style="margin-bottom: 5px;">{{$language->language->name}}</span>
                                 @endforeach
                             </div>
                         </div>
@@ -165,23 +189,29 @@
                     <div class="card card-bordered">
                         <div class="card-header bg-white border-bottom"><b>Analytics</b></div>
                         <div class="card-body" style="min-height: 325px;">
-                            <div class="row" style="margin-top: 60px;">
-                                <div class="col-md-6">
-                                    <h6 class="ndf"><em class="icon ni ni-briefcase"></em> Jobs <br style="color: #000;"> {{$number_of_jobs}}</h6>
-                                    <br>
+                            <div class="row" style="    margin-top: 70px;">
+                                <div class="col-md-6 col-lg-6 col-sm-6 col-xs-6">
+                                    <div class="text-center"><small><b>Number of Jobs</b></small></div>
+                                    <div class="text-center text-primary tex"
+                                         style="font-size: 28px;">{{$number_of_jobs}}</div>
                                 </div>
-                                <div class="col-md-6">
-                                    <h6 class="nk-blosdfck-title pafge-title"><em class="icon ni ni-activity-alt"></em> Activities <br style="color: #000;"> {{$number_of_activities}}</h6>
+                                <div class="col-md-6 col-lg-6 col-sm-6 col-xs-6">
+                                    <div class="text-center"><small><b>Number of Activities</b></small></div>
+                                    <div class="text-center text-success"
+                                         style="font-size: 28px;">{{$number_of_activities}}</div>
                                 </div>
                             </div>
 
-                            <div class="row" style="margin-top: 20px;">
-                                <div class="col-md-6">
-                                    <h6><em class="icon ni ni-star-half-fill"></em> Average Rating <br style="color: #000;"> {{$user->rating}}</h6>
-                                    <br>
+                            <div class="row">
+                                <div class="col-md-6 col-lg-6 col-sm-6 col-xs-6">
+                                    <div class="text-center"><small><b>Average Rating</b></small></div>
+                                    <div class="text-center text-primary tex"
+                                         style="font-size: 28px;">{{$user->rating}}</div>
                                 </div>
-                                <div class="col-md-6">
-                                    <h6><em class="icon ni ni-clock"></em> Volunteer Hours <br style="color: #000;"> {{$volunteer_hours}}</h6>
+                                <div class="col-md-6 col-lg-6 col-sm-6 col-xs-6">
+                                    <div class="text-center"><small><b>Volunteer Hours</b></small></div>
+                                    <div class="text-center text-success"
+                                         style="font-size: 28px;">{{$volunteer_hours}}</div>
                                 </div>
                             </div>
                         </div>
@@ -202,7 +232,8 @@
                                                       style="font-size: 22px;"></em></span></a>
                 </div>
                 <div class="card-body border-top" style="padding-top: 15px;padding-bottom: 10px;">
-                    <a id="jobExperienceLink" href="javascript:void(0)" class="text-muted">Job Experience Outside VORK <span
+                    <a id="jobExperienceLink" href="javascript:void(0)" class="text-muted">Job Experience Outside VORK
+                        <span
                             style="float: right;"><em class="icon ni ni-chevron-right"
                                                       style="font-size: 22px;"></em></span></a>
                 </div>
@@ -212,7 +243,8 @@
                                                       style="font-size: 22px;"></em></span></a>
                 </div>
                 <div class="card-body border-top" style="padding-top: 15px;padding-bottom: 10px;">
-                    <a id="educationLink" href="javascript:void(0)" class="text-muted">Education <span style="float: right;"><em
+                    <a id="educationLink" href="javascript:void(0)" class="text-muted">Education <span
+                            style="float: right;"><em
                                 class="icon ni ni-chevron-right" style="font-size: 22px;"></em></span></a>
                 </div>
             </div>
@@ -268,7 +300,8 @@
                                         <tbody>
                                         @if($number_of_jobs <= 0)
                                             <tr>
-                                                <td colspan="3"><p class="text-center">You have no completed jobs at the moment</p></td>
+                                                <td colspan="3"><p class="text-center">You have no completed jobs at the
+                                                        moment</p></td>
                                             </tr>
                                         @else
                                             @foreach($job_history as $work)
