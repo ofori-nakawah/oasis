57fa6794150765e6b5b59a99245c8b9fb49f90ab
Bernard Ofori <ofori.nakawah@gmail.com>
2023-06-07 00:44:06 +0000

    change endpoint

--- routes/api.php
+++ routes/api.php
@@ -71,7 +71,7 @@ Route::prefix('/v1')->group(function () {
              * other user profile routes
              */
             Route::get('/get-user-vork-profile', 'Mobile\UserController@get_user_vork_profile');
-            Route::get('/get-toolbox-user', 'Mobile\UserController@isToolboxUser');
+            Route::get('/is-toolbox-user', 'Mobile\UserController@isToolboxUser');
 
         });
 
