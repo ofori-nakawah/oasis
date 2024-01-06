63bade648799c168f04f627325be217c3f35f6ee
ofori-nakawah <ofori.nakawah@gmail.com>
2024-01-06 19:36:50 +0000

    add routes and controller blueprint

---
+++ app/Http/Controllers/Web/OutsideVorkJobController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
+
+class OutsideVorkJobController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function verifyReference($id)
+    {
+
+    }
+
+    public function getReferenceVerification($id)
+    {
+
+    }
+}
