78272a1cf97e13f5189ce5c2013eb49ef095a710
ofori-nakawah <ofori.nakawah@gmail.com>
2024-01-09 16:17:05 +0000

    ok

--- resources/views/emails/externalJobReferenceApprovalRequestEmail.blade.php
+++ resources/views/emails/externalJobReferenceApprovalRequestEmail.blade.php
@@ -1,3 +1,27 @@
-<div>
-    hello world
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container my-5">
+    <h1>Hello, world!</h1>
+    <div class="col-lg-8 px-0">
+        <p class="fs-5">You've successfully loaded up the Bootstrap starter example. It includes <a href="https://getbootstrap.com/">Bootstrap 5</a> via the <a href="https://www.jsdelivr.com/package/npm/bootstrap">jsDelivr CDN</a> and includes an additional CSS and JS file for your own code.</p>
+        <p>Feel free to download or copy-and-paste any parts of this example.</p>
+
+        <hr class="col-1 my-4">
+
+        <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
+        <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
+    </div>
 </div>
+</body>
+</html>
