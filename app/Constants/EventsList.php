67747eb9665ed56786870d62d583f0dac599d59e
ofori-nakawah <ofori.nakawah@gmail.com>
2024-09-29 21:24:18 +0000

    fix issues

--- app/Constants/EventsList.php
+++ app/Constants/EventsList.php
@@ -11,4 +11,18 @@ enum EventsList: string
         case QUOTE_SUBMITTED = 'QUOTE_SUBMITTED';
         case QUOTE_RECEIVED = 'QUOTE_RECEIVED';
         case SUCCESSFUL_JOB_APPLICATION = 'SUCCESSFUL_JOB_APPLICATION';
+
+        public function toString(): string
+        {
+                return match ($this) {
+                        self::APPLICATION_CONFIRMED => 'APPLICATION_CONFIRMED',
+                        self::APPLICATION_DECLINED => 'APPLICATION_DECLINED',
+                        self::SUCCESSFUL_JOB_APPLICATION  => 'SUCCESSFUL_JOB_APPLICATION',
+                        self::JOB_CLOSED  => 'JOB_CLOSED',
+                        self::QUOTE_SUBMITTED  => 'QUOTE_SUBMITTED',
+                        self::QUOTE_RECEIVED  => 'QUOTE_RECEIVED',
+                        self::JOB_CANCELLED  => 'JOB_CANCELLED',
+                        default => '',
+                };
+        }
 }
