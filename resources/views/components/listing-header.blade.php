26d07e0281294054456a23f038a5419cf1e3c4d8
ofori-nakawah <ofori.nakawah@gmail.com>
2025-04-10 10:53:07 +0000

    fix back routes

--- resources/views/components/listing-header.blade.php
+++ resources/views/components/listing-header.blade.php
@@ -1,3 +1,21 @@
+
+@php
+        $route = '';
+        switch ($type){
+                case 'Quick Jobs':
+                        $route = route('user.work.jobs', ['type_of_user' => 'seeker', 'type_of_work' => 'quick-job']);
+                        break;
+                case 'Part Time Jobs':
+                        $route = route('user.work.jobs', ['type_of_user' => 'seeker', 'type_of_work' => 'fixed-term']);
+                        break;
+                case 'Permanent Jobs':
+                        $route = route('user.work.jobs', ['type_of_user' => 'seeker', 'type_of_work' => 'permanent']);
+                        break;
+                default:
+                        $route = route('user.work.jobs', ['type_of_user' => 'seeker', 'type_of_work' => 'quick-job']);
+                        break;
+        }
+@endphp
 <div class="nk-block-head nk-block-head-sm">
         <div class="nk-block-between">
                 <div class="nk-block-head-content">
@@ -14,8 +32,8 @@
                         </div>
                 </div>
                 <div class="nk-block-head-content">
-                        <a href="{{URL::previous()}}"
-                                class="btn btn-outline-lighter"><span>Back</span></a></li>
+                        <a href="{{$route}}"
+                                class="btn btn-outline-lighter"><span>Back</span></a>
                 </div>
         </div>
 </div>
